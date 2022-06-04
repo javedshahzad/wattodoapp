bd30d845fcd84e1c84cb137decc76ba7d8f66a0e
javedshahzad <javedshahzadnns@gmail.com>
2022-06-04 18:26:06 +0500

    App created

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,14 +8,16 @@ import { AppService } from '../services/app.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  allcards:any=[];
+  allcards:any="";
   isSearch:boolean=false;
   searchdata: any=[];
   filterTerm: any;
   searchedData: any=[];
   maximum:any="";
   minimum:any="";
-
+  cardIndex=0;
+  singlecard: any=[];
+  cardIndex_forlenght: any=1;
   constructor(
     private appservice:AppService,
     private fireDB:AngularFirestore,
@@ -28,8 +30,10 @@ export class HomePage {
     this.appservice.startloading();
     this.fireDB.collection("cards").valueChanges().subscribe((res:any)=>{
       if(res){
+        console.log(res)
         this.searchdata=res;
-        this.allcards = res;
+        this.singlecard=res;
+        this.allcards = this.singlecard[this.cardIndex];
       }
       // let temp=[];
       // res.forEach(data=>{
@@ -41,56 +45,82 @@ export class HomePage {
     })
   }
   onSwipeLeft(event,item){
-    if(item.favouriteId){
+    // if(item.favouriteId){
+  
+      if(this.cardIndex === 0){
+        this.appservice.toast('This is first card');
+      }else{
+        this.cardIndex--;
+        this.cardIndex_forlenght--;
+        this.allcards = this.singlecard[this.cardIndex];
+        console.log(item);
+        console.log("Left event");
+        //this.removeFavourite(item);
+      }
+ 
+
+    // }else{
+    //   this.appservice.toast('This card is not added in favourite. Please add it to remove');
+    // }
+  }
+  onSwipeRight(event,item){
+    console.log(this.cardIndex_forlenght,this.singlecard.length);
+    if(this.cardIndex_forlenght === this.singlecard.length){
+      this.appservice.toast('This is last card');
+    }else{
+      this.cardIndex++;
+      this.cardIndex_forlenght++;
+      this.allcards = this.singlecard[this.cardIndex];
       console.log(item);
-      console.log("Left event");
-      this.removeFavourite(item)
+      console.log("right event");
+      // this.addFavourite(item);
+    }
+   
+  }
+
+  addFavourite(event,item){
+    if(item.favouriteId){
+      this.appservice.toast('This card already added in favourites');
+    }else{
+      this.appservice.startloading();
+      this.fireDB.collection('favourite').add({
+          'userId':localStorage.getItem('uid'),
+          'cardname':item.cardname,
+          'description':item.description,
+          'price':item.price,
+          'location':item.location,
+          'category':item.category,
+          'imageUrl':item.imageUrl,
+          'cardId':item.cardId,
+          'createdAt':Date.now()
+      }).then((data)=>{
+        this.fireDB.collection('cards').doc(item.cardId).update({
+          'favouriteId':data.id
+        });
+        this.fireDB.collection('favourite').doc(data.id).update({
+          'favouriteId':data.id
+        });
+        this.appservice.dismisloading();
+        console.log(data);
+        this.appservice.toast("Added to Favourites successfull");
+  
+      })
+    }
+   
+
+  }
+  removeFavourite(event,item){
+    console.log(item);
+    if(item.favouriteId){
+      this.fireDB.collection('favourite').doc(item.favouriteId).delete();
+      this.fireDB.collection('cards').doc(item.cardId).update({
+        'favouriteId':""
+      });
+      this.appservice.toast("Removed from Favourite")
     }else{
       this.appservice.toast('This card is not added in favourite. Please add it to remove');
     }
-  }
-  onSwipeRight(event,item){
-  if(item.favouriteId){
-    this.appservice.toast('This card already added in favourites');
-  }else{
-    console.log(item);
-    console.log("right event");
-    this.addFavourite(item);
-  }
-  }
 
-  addFavourite(item){
-    this.appservice.startloading();
-    this.fireDB.collection('favourite').add({
-        'userId':localStorage.getItem('uid'),
-        'cardname':item.cardname,
-        'description':item.description,
-        'price':item.price,
-        'location':item.location,
-        'category':item.category,
-        'imageUrl':item.imageUrl,
-        'cardId':item.cardId,
-        'createdAt':Date.now()
-    }).then((data)=>{
-      this.fireDB.collection('cards').doc(item.cardId).update({
-        'favouriteId':data.id
-      });
-      this.fireDB.collection('favourite').doc(data.id).update({
-        'favouriteId':data.id
-      });
-      this.appservice.dismisloading();
-      console.log(data);
-      this.appservice.toast("Added to FAavourites successfull");
-
-    })
-
-  }
-  removeFavourite(item){
-    this.fireDB.collection('favourite').doc(item.favouriteId).delete();
-    this.fireDB.collection('cards').doc(item.cardId).update({
-      'favouriteId':""
-    });
-    this.appservice.toast("Removed from Favourite")
   }
   applyfilter(){
     this.isSearch = !this.isSearch;
@@ -104,11 +134,12 @@ export class HomePage {
     //let x = arrdata.filter((a)=>{if(a.title==str2){return a}});
     const x =arrdata.filter((a)=>a.location.toUpperCase().includes(str.toUpperCase()) || a.category.toUpperCase().includes(str.toUpperCase()) || a.cardname.toUpperCase().includes(str.toUpperCase()));
     console.log(x);
-    this.searchedData=x;
-    if(this.searchedData.length !== 0){
-      this.allcards=this.searchedData;
+    this.cardIndex=0;
+    this.singlecard=x;
+    if(this.singlecard.length !== 0){
+      this.allcards=this.singlecard[this.cardIndex];
     }else{
-      this.allcards=x;
+      this.allcards="";
       this.appservice.toast('Data not Found!');
     }
     console.log(this.allcards);
@@ -127,11 +158,12 @@ export class HomePage {
     //let x = arrdata.filter((a)=>{if(a.title==str2){return a}});
     const x =arrdata.filter((a)=> a.price >= min && a.price <=max);
     console.log(x);
-    this.searchedData=x;
-    if(this.searchedData.length !== 0){
-      this.allcards=this.searchedData;
+    this.cardIndex=0;
+    this.singlecard=x;
+    if(this.singlecard.length !== 0){
+      this.allcards=this.singlecard[this.cardIndex];
     }else{
-      this.allcards=x;
+      this.allcards="";
       this.appservice.toast('Data not Found!');
     }
     }
@@ -142,11 +174,12 @@ export class HomePage {
     //let x = arrdata.filter((a)=>{if(a.title==str2){return a}});
     const x =arrdata.filter((a)=> a.price >= str );
     console.log(x);
-    this.searchedData=x;
-    if(this.searchedData.length !== 0){
-      this.allcards=this.searchedData;
+    this.cardIndex=0;
+    this.singlecard=x;
+    if(this.singlecard.length !== 0){
+      this.allcards=this.singlecard[this.cardIndex];
     }else{
-      this.allcards=x;
+      this.allcards="";
       this.appservice.toast('Data not Found!');
     }
     }else if(this.maximum){
@@ -156,11 +189,12 @@ export class HomePage {
     //let x = arrdata.filter((a)=>{if(a.title==str2){return a}});
     const x =arrdata.filter((a)=> a.price <= str );
     console.log(x);
-    this.searchedData=x;
-    if(this.searchedData.length !== 0){
-      this.allcards=this.searchedData;
+    this.cardIndex=0;
+    this.singlecard=x;
+    if(this.singlecard.length !== 0){
+      this.allcards=this.singlecard[this.cardIndex];
     }else{
-      this.allcards=x;
+      this.allcards="";
       this.appservice.toast('Data not Found!');
     }
     }
